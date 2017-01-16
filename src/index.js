a13aa59d9c8542429cb1a5dc9a32984cf15523cb
Jonathan Lam <jonathanlam@Jonathans-MacBook.local>
2017-01-15 17:05:15 -0800

    Add video selection functionality to the video list items

--- src/index.js
+++ src/index.js
@@ -18,13 +18,20 @@ const API_KEY = APIKEY.YOUTUBE_API_KEY;
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     // *** Downwards Data Flow : We want the most parent component to be responsible for fetching the data. ***
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       // when we have a key and a value of the same terms, we can use new ES6 syntax.
       console.log(videos);
-      this.setState({videos}); //this.setState({videos: videos})
+      //this.setState({videos}); //this.setState({videos: videos})
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
 
   }
@@ -33,8 +40,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
